50019182db8efb55797bea9f0496fdef86562269
bruteF04C3 <rishabhjhacr07@gmail.com>
2021-11-14 15:08:57 +0530

    response rec from api

--- script.js
+++ script.js
@@ -16,18 +16,33 @@ function getCountryName(val) {
     country = val;
 }
 
-function getResult() {
+function showNextCard(prevCard) {
+    if (prevCard == 'city-name') {
+        document.getElementById('city-card').style.display = 'none';
+        document.getElementById('state-card').style.display = 'block';
+    } else {
+        document.getElementById('state-card').style.display = 'none';
+        document.getElementById('country-card').style.display = 'block';
+    }
+
+}
+
+function getResult() {    
     url = `http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=${country}&key=${API_KEY}`;
     fetch(url)
-        .then(response => {
-            response.json()
+        .then((response) => {
+            return response.json()
         })
-        .then(response => {
-            console.log(response)
+        .then((json) => {
+            renderResults(json);
         })
         .catch(error => {
             console.error(error);
         });
 
+}
+
+function renderResults(data) {
+    
 }
 
